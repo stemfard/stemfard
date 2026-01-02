fdaf2b70a472b29b310979f7be6b69f33f67a596
stemxresearch <stemxresearch@gmail.com>
2026-01-02 12:54:21 +0300

    Commit

--- src/stemfard/stats/eda.py
+++ src/stemfard/stats/eda.py
@@ -1,16 +1,44 @@
 from typing import Literal
 
-from numpy import ndarray
+from numpy import array, cumsum, int64, ndarray
+from pandas import DataFrame
 
 
 def sta_eda_grouped(
     lc_limits: list[int | float] | ndarray,
     uc_limits: list[int | float] | ndarray,
     freq: list[int | float] | ndarray,
-    statistic: Literal["mean", "sd", "percentiles"]
+    statistic: Literal["mean", "sd", "percentiles"],
+    decimals: int = 4
 ) -> float:
     
-    return freq
+    if not isinstance(lc_limits, ndarray):
+        lc_limits = array(lc_limits)
+    
+    if not isinstance(uc_limits, ndarray):
+        uc_limits = array(uc_limits)
+        
+    if not isinstance(freq, ndarray):
+        freq = array(freq, dtype=int64)
+    
+    n = len(lc_limits)
+    class_limits = [
+        f"{lc_limits[index]} - {uc_limits[index]}" for index in range(n)
+    ]
+    
+    df_series = {
+        "Class": class_limits,
+        "Frequency": freq
+    }
+    
+    if statistic == "percentiles":
+        df_series.update(
+            {"Cumulative frequency": cumsum(freq)}
+        )
+    
+    dframe = DataFrame(data=df_series)
+    
+    return dframe
 
 
 def sta_eda_ungrouped(
